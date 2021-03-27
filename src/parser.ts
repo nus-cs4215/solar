9a178ff9e13ffd093e76252d53a3a887343bba11
Alex <alxkohh@gmail.com>
2021-03-27 12:51:02 +0800

    Add luaparse parser

---
+++ src/parser.ts
@@ -0,0 +1,19 @@
+const parser = require('luaparse');
+
+const prog = `
+
+-- this is a comment
+
+function foo(x)
+    print(x)
+    return 5
+end
+
+foo(3)
+
+`;
+
+const ast = parser.parse(prog);
+
+console.log(JSON.stringify(ast));
+console.log(ast);
