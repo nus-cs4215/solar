c456e0d6b78b3acc9da9810abc6799e08c702e44
Alex <alxkohh@gmail.com>
2021-03-27 12:58:01 +0800

    Edit sample program

--- src/parser.ts
+++ src/parser.ts
@@ -9,7 +9,7 @@ const prog = `
 
 function foo(x)
     print(x)
-    return 5
+    return 5 + 5
 end
 
 foo(3)
@@ -18,7 +18,7 @@ foo(3)
 
 const ast = parser.parse(prog);
 
-console.log(JSON.stringify(ast));
-console.log(ast);
+console.log(JSON.stringify(ast));   // prints the full AST in string form
+console.log(ast);                   // prints the concise AST in object form
 
 // to run this file - npm run parse
