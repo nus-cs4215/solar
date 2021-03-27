ce47990bf1ef0470af66b6dcc118a120cf7316b3
Alex <alxkohh@gmail.com>
2021-03-27 14:33:22 +0800

    eval chunk

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -1,10 +1,18 @@
 export class Evaluator {
+
+    globalScope: Scope = new Scope({}, null);
+
     isChunk(component: any): boolean {
         return component.type === 'Chunk';
     }
 
-    evalChunk(component: any): string | number | boolean | null {
-        return this.evaluate(component.body);
+    evalChunk(component: any, scope: Scope): void {
+        
+        const body = component.body;
+        
+        for (let c of body) {
+            this.evaluate(c);
+        }
     }
 
     isLiteral(component: any): boolean {
@@ -21,7 +29,7 @@ export class Evaluator {
     evaluate(component: any): any {
         // number, boolean, string, nil
         return this.isChunk(component)
-            ? this.evalChunk(component)
+            ? this.evalChunk(component, this.globalScope)
             : this.isLiteral(component)
             ? this.evalLiteral(component)
             : console.error("Unknown component type");
