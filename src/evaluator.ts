f5b788acf4eba2fb6e4a9c9f988b0d34cfd088b3
Alex <alxkohh@gmail.com>
2021-03-27 13:46:06 +0800

    Remove outdated code in evaluator

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -1,37 +1,9 @@
 /*
     AST interpreter for Solar.
-    Evaluation of the AST is done recursively.
-
-    Current progress:
-    - evaluate literal
-    - evaluate arithmetic
-
+    Since the AST is a recursive data structure, our strategy is to evaluate it recursively.
 */
 export class Evaluator {
     
-    typeIsArithmetic(tree: any): boolean {
-        return tree.type === 'plus' || tree.type === 'minus' || tree.type === 'times' || tree.type === 'divide'
-    }
-
-    arithmeticEval(tree: any): number {
-        
-        if (tree.type === 'plus') {
-            return this.evaluate(tree.left) + this.evaluate(tree.right);
-        }
-        
-        if (tree.type === 'minus') {
-            return this.evaluate(tree.left) - this.evaluate(tree.right);
-        }
-
-        if (tree.type === 'times') {
-            return this.evaluate(tree.left) * this.evaluate(tree.right);
-        }
-
-        if (tree.type === 'divide') {
-            return this.evaluate(tree.left) / this.evaluate(tree.right);
-        }
-    }
-
     /*
         typescript has an 'eval' function, hence we use the 'evaluate' keyword to prevent name clash
         for the param, we use tree, instead of AST!
@@ -39,13 +11,8 @@ export class Evaluator {
     */
     evaluate(tree: any): any {
         
-        // number, boolean, string
-        if (tree.type === 'literal') {
-            return tree.value;
-        }
-
-        if (this.typeIsArithmetic(tree)) {
-            return this.arithmeticEval(tree);
-        }
     }
+    
+
+
 }
