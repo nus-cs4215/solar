c3fa7546b779a4111ad34ba8dead35b756610aa4
Alex <alxkohh@gmail.com>
2021-04-01 23:30:29 +0800

    Restructure main file

--- src/interpreter/main.ts
+++ src/interpreter/main.ts
@@ -3,6 +3,16 @@ const parser = require('luaparse');
 
 // To run this file - npm start
 
+function interpret(program: string): any {
+
+    // parse program into AST
+    const ast = parser.parse(program, { luaVersion: '5.3' });
+    
+    // evaluate AST
+    const e = new Evaluator();
+    e.evaluate(ast);
+}
+
 
 // user program
 const prog = `
@@ -11,11 +21,4 @@ print('hi')
 
 `;
 
-// parse program into AST
-const ast = parser.parse(prog, { luaVersion: '5.3' });
-
-//console.log(ast.body);
-
-// evaluate AST
-const e = new Evaluator();
-e.evaluate(ast);
+interpret(prog);
