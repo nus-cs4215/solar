e6db18fcbcc172189c7b136ae8ee7ff007678e30
Alex <alxkohh@gmail.com>
2021-04-01 14:43:51 +0800

    Add ide to src

---
+++ src/ide/lib/js/editor.js
@@ -0,0 +1,77 @@
+// Retrieve Elements
+const consoleLogList = document.querySelector('.editor__console-logs');
+const executeCodeBtn = document.querySelector('.editor__run');
+const resetCodeBtn = document.querySelector('.editor__reset');
+
+// Setup Ace
+let codeEditor = ace.edit("editorCode");
+let consoleMessages = [];
+
+let editorLib = {
+    clearConsoleScreen() {
+        consoleMessages.length = 0;
+
+        // Remove all elements in the log list
+        while (consoleLogList.firstChild) {
+            consoleLogList.removeChild(consoleLogList.firstChild);
+        }
+    },
+    printToConsole() {
+        consoleMessages.forEach(log => {
+            const newLogItem = document.createElement('li');
+            const newLogText = document.createElement('pre');
+
+            newLogText.className = log.class;
+            newLogText.textContent = `> ${log.message}`;
+
+            newLogItem.appendChild(newLogText);
+            consoleLogList.appendChild(newLogItem);
+        })
+    },
+    init() {
+        // Configure Ace
+
+        // Theme
+        codeEditor.setTheme("ace/theme/dreamweaver");
+
+        // Set language
+        codeEditor.session.setMode("ace/mode/lua");
+
+        // Set Options
+        codeEditor.setOptions({
+            fontSize: '12pt',
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+        });
+
+        // Set Default Code
+        // codeEditor.setValue(defaultCode);
+    }
+}
+
+// Events
+executeCodeBtn.addEventListener('click', () => {
+    // Clear console messages
+    editorLib.clearConsoleScreen();
+
+    // Get input from the code editor
+    const userCode = codeEditor.getValue();
+
+    // Run the user code
+    try {
+        // console.log(typeof userCode);
+        console.log(userCode);
+    } catch (err) {
+        console.error(err);
+    }
+
+    // Print to the console
+    editorLib.printToConsole();
+});
+
+resetCodeBtn.addEventListener('click', () => {
+    // Clear console messages
+    editorLib.clearConsoleScreen();
+})
+
+editorLib.init();
