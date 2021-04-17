fac5677a18431ae290cebf56119403757197f033
Alex <alxkohh@gmail.com>
2021-04-17 18:48:59 +0800

    Add test

--- tests/print.test.ts
+++ tests/print.test.ts
@@ -1,7 +1,7 @@
 import { interpret } from '../src/interpreter/main';
 import { getLog } from 'console-testing-library';
 
-test(`testing two calls to print()`, () => {
+test('2 print statements', () => {
 
     const prog = `
     print('hello world') print('hi')
@@ -9,4 +9,28 @@ test(`testing two calls to print()`, () => {
     interpret(prog);
     expect(getLog().log).toBe('hello world\nhi');
 }
-)
+);
+
+test('print(true)', () => {
+
+    const prog = `
+    print(true)
+    `;
+    interpret(prog);
+    expect(getLog().log).toBe('true');
+}
+);
+
+test('print loop variable i inside a for loop', () => {
+
+    const prog = `
+    
+    for i = 0, 6, 1 do
+        print(i)
+    end
+    
+    `;
+    interpret(prog);
+    expect(getLog().log).toBe('0\n1\n2\n3\n4\n5');
+}
+);
